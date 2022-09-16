362b9fe1fb948db655bcde7eb74dffd93b32a637
JoseRoj <jorojas2019@inf.udec.cl>
2022-09-16 19:42:52 -0300

    not error

--- main.cpp
+++ main.cpp
@@ -7,7 +7,10 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <algorithm>
+#include <libexplain/execvp.h>
+
 //#include <bits/stdc++.h>
+//typedef cdo =  'cd /home/test/' 
 
 
 const int READ = 0; // variable para pipe
@@ -18,11 +21,9 @@ int main()
 {
     vector<vector<string> > All;
     vector<string> command;
-    //cin >> cadena;
-    //cout << cadena;
     string instruction;
     bool pront = true;
-    //cout << "$";
+    cout << "$";
     long long maxim;
     while (getline(cin,instruction)){ 
         maxim = 0;
@@ -57,16 +58,20 @@ int main()
                 ArgsCommand[i] = strdup(All[0][i].c_str());
             }
             ArgsCommand[i] = NULL;
+            
             int pid = fork();
             if(pid == 0){        
-                execvp(ArgsCommand[0],ArgsCommand);
-                exit(EXIT_FAILURE);
+                pront = true;
+                int exeret = execvp(ArgsCommand[0],ArgsCommand);
+                fprintf(stderr, "%s\n", Explain_execvp(ArgsCommand[0],ArgsCommand));
+                exit(errno);
             }else if(pid < 0){
                 cout << "ERROR";
                 // ERROR;
             }
             else{
-                wait(NULL);
+                //wait(NULL);
+
             }
         }else{      // Case find pipe
             char *ArgsCommand[All.size()][maxim + 1];
@@ -74,94 +79,88 @@ int main()
                 for (int j = 0; j < All[i].size(); j++)
                 {
                     ArgsCommand[i][j] = strdup(All[i][j].c_str()); 
-                    cout << ArgsCommand[i][j] << " ";
+                    //cout << ArgsCommand[i][j] << " ";
                 }
                 ArgsCommand[i][All[i].size()] = NULL;
             }
-            // CREATE PIPES
+    //-------------------  INICIALIZED PIPES -----------------------------//
+
             long long allpipes  = All.size() - 1;
             int Pipes[allpipes][2];
-            //vector<vector<int>> Pipes(All.size() - 1,n) ;
             for(int k = 0; k < allpipes; k++)  pipe(Pipes[k]);
                     
                     // CHILDREN PROCESS FOR THE PIPES
             int count  = 0;
-            if(allpipes == 1){
-                if(fork() == 0){
-                    close(Pipes[count][READ]);
-                    dup2(Pipes[count][WRITE], WRITE);
-                    execvp(ArgsCommand[0][0], ArgsCommand[0]);
-                }else{
-                    close(Pipes[count][READ]);
-                    dup2(Pipe[cout][WRITE]);
-                    execvp(ArgsCommand)
-                }
-            }else{
-            // PROCESs 1
-            int count = 0;
+
+
+    // ----------------------------- FIRST PROCESS -----------------------//               
             if(fork() == 0){
-                dup2(Pipes[count][WRITE],WRITE);
-                close(Pipes[count][READ]); // abrir canal de lectura
-                // CLOSE OTHERS PIPES
+                dup2(Pipes[0][WRITE],WRITE); // abrir canal de lectura
+                close(Pipes[0][READ]);
                 int path = 1;
                 while(path < allpipes){
                     close(Pipes[path][READ]);
                     close(Pipes[path][WRITE]);
+                    path++;
                 }
                 execvp(ArgsCommand[0][0],ArgsCommand[0]); 
-                printf("error1");  
-                // PROCESO 2
-            }
-            count++;
-            if(allpipes > 1){
-                cout << "Entre" << endl;
-                while(count < allpipes - 1){
-                    if(fork()==0){
-                        dup2(Pipes[count - 1][READ],READ); // abrir canal de lectura
-                        dup2(Pipes[count][WRITE],WRITE); // abrir canal de escritura
-                        for(int i = 0; i < allpipes; i++){
-                            if(i == count - 1)
-                                close(Pipes[i][WRITE]);
-                            else if(i == count) 
-                                close(Pipes[i][READ]);
-                            else{
-                                close(Pipes[i][READ]);
-                                close(Pipes[i][WRITE]);
-                            }
-                        }
-                        execvp(ArgsCommand[count][0],ArgsCommand[count]); 
-                        printf("error2");
-                    }
-                    count++;
+                printf("error1");
                 }
-            }
-            if(fork()==0){
-                // PROCESO 3
-                dup2(Pipes[count][READ],READ); // abrir canal de lectura
-                close(Pipes[count][WRITE]);
-                int path = 0;
-                //el proceso 3 no utiliza el pipe1
-                while(path < allpipes){
-                    if(path == count) continue;
-                    close(Pipes[path][READ]);
-                    close(Pipes[path][WRITE]);
-                }
-                cout << ArgsCommand[1] << endl;
-                execvp(ArgsCommand[count][0],ArgsCommand[count]); 
-                printf("error3");
-            }
-            //cout << "count : " << count << endl;
-            for (int i = 0; i < allpipes; i++){
-                close(Pipes[i][WRITE]);
-                close(Pipes[i][READ]);                
-            }
-            for(int l = 0 ; l < allpipes; l++) wait(NULL);
 
-        }
+    //----------------------------------  PROCESS 2 OR MORE --------------------------//
+
+                if(allpipes > 1){
+                    // PROCESO 2
+                    while(count < allpipes - 1){
+                        if(fork()==0){
+                            dup2(Pipes[count][READ],READ); // abrir canal de lectura
+                            dup2(Pipes[count + 1][WRITE],WRITE); // abrir canal de escritura
+                            for(int i = 0; i < allpipes; i++){
+                                if(i == count)
+                                    close(Pipes[i][WRITE]);
+                                else if(i == count + 1) 
+                                    close(Pipes[i][READ]);
+                                else{
+                                    close(Pipes[i][READ]);
+                                    close(Pipes[i][WRITE]);
+                                }
+                            }
+                            execvp(ArgsCommand[count + 1][0],ArgsCommand[count + 1]); 
+                            printf("error2");
+                        }
+                        count++;
+                    }
+                }
+
+    // ----------------------------------- FINAL PROCESS ----------------------------------// 
+
+                if(fork()==0){
+                    // PROCESO 3
+                    dup2(Pipes[count][READ],READ); // abrir canal de lectura
+                    close(Pipes[count][WRITE]);
+                    int path = 0;
+                    //el proceso 3 no utiliza el pipe1
+                    while(path < allpipes){
+                        if(path == count){
+                            path++;
+                            continue;
+                        }
+                        close(Pipes[path][READ]);
+                        close(Pipes[path][WRITE]);
+                        path++;
+                    }
+                    execvp(ArgsCommand[count + 1][0],ArgsCommand[count + 1]); 
+                    printf("error3");
+                }
+                for (int i = 0; i < allpipes; i++){
+                    close(Pipes[i][WRITE]);
+                    close(Pipes[i][READ]);                
+                }
+                for(int l = 0 ; l < All.size(); l++) wait(NULL);
+            }
+        
         All.clear();   
         pront = true;         
-
-
     }
     return 0;
 }
